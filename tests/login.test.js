37ade957cb39c87146dceabde05a48989e555952
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-04-09 21:27:57 +0530

    upd login test workflow

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,23 +1,27 @@
 import 'dotenv/config'
 import {exec} from 'child_process'
 import util from 'util'
+import { OK } from '../src/constants'
 
 const execAsync = util.promisify(exec)
 
 const TestLogin = async()=>{
     try{const command = `
-    curl --location '${process.env.BASE_URL}/users/login' \
+    curl --silent --location \
+     -w "\\n%{http_code}" \
+    '${process.env.BASE_URL}/users/login' \
     -H 'Content-Type: application/json' \
     -d '{
         "username" : "${process.env.TEST_USERNAME}",
         "password": "${process.env.TEST_PASSWORD}"
     }'`
-     const { stdout, stderr } = await execAsync(command)
-     if (stderr) {
-      throw new Error(`Curl error: ${stderr}`)
+    const { stdout} = await execAsync(command)
+    const [body, status] = stdout.split('\n')
+    const response = JSON.parse(body)
+     if (status !== `${OK}`) {
+      throw new Error(`Status ${status}: ${JSON.stringify(response)}`)
     }
 
-    const response = JSON.parse(stdout)
     console.log('Login test passed!', response)}
     catch(err){
          console.error('Login test failed:', err.message)

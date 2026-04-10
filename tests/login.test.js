197e9cda5bca2eac78a07f779dc879408842c040
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-04-10 20:08:20 +0530

    upd login test workflow

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,32 +1,29 @@
 import 'dotenv/config'
-import {exec} from 'child_process'
-import util from 'util'
+// import {exec} from 'child_process'
+// import util from 'util'
 
-const execAsync = util.promisify(exec)
+// const execAsync = util.promisify(exec)
 
 const TestLogin = async()=>{
     try{
-    const command = `
-    curl --silent --location \
-    -w "\\n%{http_code}" \
-    '${process.env.BASE_URL}/api/${process.env.API_VERSION}/users/login'
-    -H 'Content-Type: application/json' \
-    -d '{
-        "username" : "${process.env.TEST_USERNAME}",
-        "password": "${process.env.TEST_PASSWORD}"
-    }'`
+    const url = `${process.env.BASE_URL}/api/${process.env.API_VERSION}/users/login`
 
-    const { stdout } = await execAsync(command)
-    const [body, status] = stdout.split('\n')
-    const response = JSON.parse(body)
-     if (status !== `200`) {
-      throw new Error(`Status ${status}: ${JSON.stringify(response)}`)
-    }
-
-    console.log('Login test passed!')}
-    catch(err){
-         console.error('Login test failed:', err.message)
-         process.exit(1)
+    const res = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                username: process.env.TEST_USERNAME,
+                password: process.env.TEST_PASSWORD
+            })
+    })
+   const response = await res.json()
+   if (!res.ok) {
+            throw new Error(`Status ${res.status}: ${JSON.stringify(response)}`)
+        }
+        console.log('Login test passed!', response.message)
+    } catch (err) {
+        console.error('Login test failed:', err.message)
+        process.exit(1)
     }
 }
 

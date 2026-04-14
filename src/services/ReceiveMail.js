d6f044d8b06f4d33f359bc40be150ff1be98650a
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-04-14 20:48:31 +0530

    test : test inbox mail feature 3

--- src/services/ReceiveMail.js
+++ src/services/ReceiveMail.js
@@ -2,18 +2,22 @@ import {Resend} from "resend"
 import { MAIL_ID } from "../constants.js"
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-const forwardMail = async({from, subject, text})=>{
+const forwardMail = async({from, subject, text, html})=>{
+    const content = text?.trim()
+    ? `<p>${text}</p>`
+    : html?.trim()
+      ? html
+      : "<p>No content</p>"
+
     await resend.emails.send({
         from: "Anirban Builds <contact@anirbanbuilds.online>",
         to : MAIL_ID,
-        subject : `email from ${from}`,
+        subject : `${subject}`,
         html: `
         <div style="font-family:sans-serif;">
-            <h2>New Incoming Email</h2>
-            <p><strong>From:</strong> ${from}</p>
-            <p><strong>Subject:</strong> ${subject}</p>
             <hr/>
-            <p>${text}</p>
+            <p><strong>From:</strong> ${from}</p>
+            <p>${content}</p>
         </div>
         `
     })
